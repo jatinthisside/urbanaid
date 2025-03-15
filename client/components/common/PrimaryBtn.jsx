f67371ea28d12cf73a30a48e3e41c48db38d8c57
jatinthisside <jatinkyadu@gmail.com>
2025-03-15 12:14:44 +0530

    Folder structure setup for react

---
+++ client/components/common/PrimaryBtn.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PrimaryBtn() {
+  return (
+    <button className='bg-primary text-white px-4 py-2 rounded-md'>Text here</button>
+  )
+}
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router';
+import Home from '../pages/Home';
+import Contact from '../pages/Contact';
+import About from '../pages/About';
+
 
 export default function App() {
   return (
     <div>
-      Hello World
+      <Routes> 
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
     </div>
   )
 }
