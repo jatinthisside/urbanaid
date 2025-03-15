569235ab94f8eb25f743f5b556493a1beddcf318
jatinthisside <jatinkyadu@gmail.com>
2025-03-15 11:46:58 +0530

    Fix :  remove all frontend code to restart

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -2,9 +2,9 @@ const User = require("../models/User.js");
 
 exports.signup = async (req, res) => {
     try {
-        const {fullname,phone,email,password,gender,role,city,state,country,pincode} = req.body
+        const {fullName:fullname,phone,email,gender,role,city,state,country,pincode} = req.body
 
-        if (!fullname || !phone || !email || !password || !gender || !city || !state || !country || !pincode) 
+        if (!fullname || !phone || !email || !gender || !city || !state || !country || !pincode) 
         {
           return res.status(403).json({
             success: false,
@@ -20,33 +20,17 @@ exports.signup = async (req, res) => {
             message: "User already exists. Please sign in to continue.",
           })
         }
-    
-        // Find the most recent OTP for the email
-        const response = await OTP.find({ $or: [{email}, {phone}] }).sort({ createdAt: -1 }).limit(1)
-        console.log(response)
-        if (response.length <= 0) {
-          return res.status(400).json({
-            success: false,
-            message: "The OTP is not valid",
-          })
-        } else if (otp !== response[0].otp) {
-          return res.status(400).json({
-            success: false,
-            message: "The OTP is not valid",
-          })
-        }
-    
-        // Hash the password
-        const hashedPassword = await bcrypt.hash(password, 10)
 
         const user = await User.create({
           fullname,
           phone,
           email,
-          password: hashedPassword,
           role,
-          is_verified: true,
-          image: "",
+          gender,
+          city,
+          state,
+          country,
+          pincode,
         })
     
         return res.status(200).json({
--- backend/models/User.js
+++ backend/models/User.js
@@ -5,15 +5,13 @@ const UserSchema = new mongoose.Schema(
     fullname: { type: String, required: true, trim: true },
     email: { type: String, unique: true, required: true, trim: true },
     phone: { type: String, required: true, trim: true, unique: true },
-    password: { type: String, required: true, trim: true },
     gender: { type: String, trim: true },
     role: { type: String, enum: ["customer", "vendor"], required: true },
     city: { type: String, trim: true },
     state: { type: String, trim: true },
     country: { type: String, trim: true },
     pincode: { type: String, trim: true },
-    profile_pic: { type: String, trim: true },
-    is_verified: { type: Boolean, default: false },
+    is_profile_completed: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
