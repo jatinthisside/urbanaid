872c298d5225213513896337dad80ebf16201f26
jatinthisside <jatinkyadu@gmail.com>
2025-03-19 18:40:31 +0530

    Refactor : remove custom validator from /models/OTP

--- backend/models/OTP.js
+++ backend/models/OTP.js
@@ -14,14 +14,6 @@ const OTPSchema = new mongoose.Schema({
     validateBeforeSave: true
 });
 
-// Custom validator to ensure either email or phone is provided
-OTPSchema.pre('save', function(next) {
-    if (!this.email && !this.phone) {
-        next(new Error('Either email or phone is required'));
-    }
-    next();
-});
-
 // Add TTL index to automatically delete documents after 10 minutes
 OTPSchema.index({ createdAt: 1 }, { expireAfterSeconds: 600 }); // 600 seconds = 10 minutes
 
