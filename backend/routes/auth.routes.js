5556515ddfaee7022e7efef987d1be242ae6fa07
jatinthisside <jatinkyadu@gmail.com>
2025-03-19 18:42:20 +0530

    Feat : add routes to send and verify otp

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,7 +1,12 @@
 const express = require("express");
-const { signup, signin } = require("../controllers/auth.controllers.js");
+const { signup, signin, sendOtp, verifyOtp } = require("../controllers/auth.controllers.js");
 const router = express.Router();
 
+// OTP routes
+router.post("/send-otp", sendOtp);
+router.post("/verify-otp", verifyOtp);
+
+// Authentication routes
 router.post("/signup", signup);
 router.post("/signin", signin);
 
