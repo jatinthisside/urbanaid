d13450970c1af60139119d55d6992d92e45db172
jatinthisside <jatinkyadu@gmail.com>
2025-03-23 19:11:57 +0530

    Refactor : add password field to user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema(
     fullname: { type: String, required: true, trim: true },
     email: { type: String, unique: true, required: true, trim: true },
     phone: { type: String, required: true, trim: true, unique: true },
+    password: { type: String, required: true, trim: true },
     gender: { type: String, trim: true },
     role: { type: String, enum: ["customer", "vendor"], required: true },
     city: { type: String, trim: true },
